f8e9b23db7fa94d45bae434161aa05162bd7e7d4
Sebastian Cohnen <tisba@users.noreply.github.com>
2018-08-17 12:04:59 +0200

    update development aliases

--- omz-custom/development.zsh
+++ omz-custom/development.zsh
@@ -1,5 +1,11 @@
+# Sublime
 export PATH="/Applications/Sublime Text.app/Contents/SharedSupport/bin/":$PATH
 
+# Erlang
 export ERL_AFLAGS="-kernel shell_history enabled"
 
+# Go
+alias gosrc='cd $GOPATH/src/github.com/'
+
+# Networking
 whoseport() { lsof -i "TCP:$1" | grep LISTEN }
