7ee8cab8bfa199a4d9b0f0c691e96f560d9db6db
Sebastian Cohnen <tisba@users.noreply.github.com>
2020-06-25 19:21:31 +0200

    changes SRC_WORK_PATH

--- omz-custom/work.zsh
+++ omz-custom/work.zsh
@@ -1,5 +1,5 @@
 # development / workspaces
-export SRC_WORK_PATH="~/Documents/work/freelancing"
+export SRC_WORK_PATH="~/Documents/work"
 alias sf="${SRC_WORK_PATH}/stormforger"
 alias sfsl="$EDITOR --project ${SRC_WORK_PATH}/stormforger/src/sf-core.sublime-project"
 alias work="cd ${SRC_WORK_PATH}"
