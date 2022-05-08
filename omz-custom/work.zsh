c66cefc6e356dfd243905cffa4cc894af754259a
Sebastian Cohnen <tisba@users.noreply.github.com>
2022-05-08 14:36:29 +0200

    simplifies work aliases

--- omz-custom/work.zsh
+++ omz-custom/work.zsh
@@ -1,4 +1,2 @@
 # development / workspaces
-local SRC_WORK_PATH="~/Documents/work"
-alias sf="${SRC_WORK_PATH}/stormforger"
-alias sfsl="$EDITOR --project ${SRC_WORK_PATH}/stormforger/src/sf-core.sublime-project"
+alias sf="~/Documents/stormforge"
