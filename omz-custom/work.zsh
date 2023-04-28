507e8806bc569996509b765fd8aab14544668c45
Sebastian Cohnen <tisba@users.noreply.github.com>
2023-04-28 10:35:16 +0200

    sets GOPRIVATE for work

--- omz-custom/work.zsh
+++ omz-custom/work.zsh
@@ -1,2 +1,6 @@
 # development / workspaces
 alias sf="~/Documents/stormforge"
+
+# Skip usage of goproxy for gramLabs and stormforger on Github
+# See https://goproxy.io/docs/GOPRIVATE-env.html
+export GOPRIVATE=github.com/gramLabs,github.com/stormforger
