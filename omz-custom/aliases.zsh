7b7142558bf163c776057339faf9dc39570f7123
Sebastian Cohnen <tisba@users.noreply.github.com>
2024-04-25 13:00:59 +0200

    cleans out old/unused aliases

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -1,10 +1,6 @@
 # remove "git status -s" alias
 unalias gss
 
-# edit configurations
-alias zshconfig="$EDITOR ~/.dotfiles"
-alias sshconfig="$EDITOR ~/.ssh/config"
-
 # basics
 alias grep="grep --color=auto"
 alias ll="ls -lsahF"
@@ -12,12 +8,9 @@ alias l="ll"
 alias l.="ls -d .[^.]*"
 alias ll.="ls -la -d .[^.]*"
 
-# ruby tools
-alias be="bundle exec"
-
 # utils
 alias rhttp="ruby -run -e httpd . -p 9090"
-alias curl_timing="curl -w \"\n\n%{http_code} -> TOTAL: %{time_total}, DNS Lookup: %{time_namelookup}, Connect: %{time_connect}, Start Transfer: %{time_starttransfer}\n\n\""
+alias curl-timing="curl -w \"\n\n%{http_code} -> TOTAL: %{time_total}, DNS Lookup: %{time_namelookup}, Connect: %{time_connect}, Start Transfer: %{time_starttransfer}\n\n\""
 alias cdpwd="cd \$(realpath \$(pwd))"
 alias rdiff="diff -rq --exclude=.DS_Store"
 
