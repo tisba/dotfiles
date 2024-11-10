cce1122a28be4cf90a83826265372209b364e130
Sebastian Cohnen <tisba@users.noreply.github.com>
2024-11-10 16:13:41 +0100

    updates rdiff alias to exclude more macOS specific meta data files

--- omz-custom/aliases.zsh
+++ omz-custom/aliases.zsh
@@ -12,7 +12,13 @@ alias ll.="ls -la -d .[^.]*"
 alias rhttp="ruby -run -e httpd . -p 9090"
 alias curl-timing="curl -w \"\n\n%{http_code} -> TOTAL: %{time_total}, DNS Lookup: %{time_namelookup}, Connect: %{time_connect}, Start Transfer: %{time_starttransfer}\n\n\""
 alias cdpwd="cd \$(realpath \$(pwd))"
-alias rdiff="diff -rq --exclude=.DS_Store"
+
+# Recursive diff of two directories, excluding:
+# * .DS_Store
+# * ._* - AppleDouble files, stores metadata on filesystems that don't support it natively
+# * .fseventsd - File System Events daemon, used by Time Machine etc
+# * .Spotlight-V100 - Spotlight index
+alias rdiff="diff -rq --exclude=.DS_Store --exclude='._*' --exclude=.fseventsd --exclude=.Spotlight-V100"
 
 # git
 alias gss="git stash show -p stash@{0}"
