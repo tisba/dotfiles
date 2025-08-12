c3c0661e7104a1a6c049d99f9b0cfeb23c8b2317
Sebastian Cohnen <tisba@users.noreply.github.com>
2025-08-12 17:05:01 +0200

    deletes more SF specific stuff

--- omz-custom/work.zsh
+++
@@ -1,6 +0,0 @@
-# development / workspaces
-alias sf="~/Documents/stormforge"
-
-# Skip usage of goproxy for gramLabs and stormforger on Github
-# See https://goproxy.io/docs/GOPRIVATE-env.html
-export GOPRIVATE=github.com/gramLabs,github.com/stormforger
