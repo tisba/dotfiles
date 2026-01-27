69562429e406c78d9b75d284075c6d60eb2f46e7
Sebastian Cohnen <tisba@users.noreply.github.com>
2026-01-27 15:27:38 +0100

    adds remark for GOPATH

--- omz-custom/go.zsh
+++ omz-custom/go.zsh
@@ -1,3 +1,6 @@
+# Control, where Go installs packages.
+# via https://go.dev/wiki/SettingGOPATH:
+#   If no GOPATH is set, it is assumed to be $HOME/go on Unix systems
 export GOPATH="$HOME/Documents/src/golang"
 
 export PATH="$GOPATH/bin:$PATH"
